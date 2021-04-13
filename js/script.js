341bc259b402346c93db57391e9ceb9648acca99
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-04-13 17:30:04 +0200

    adding of logo and title trying to create an animation for the disappearing menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-window.onscroll= () =>{
-    let opaci = document.getElementsByClassName("menu")[0].style.opacity;
-    window.alert(opaci);
+window.onload = () =>{
+    /*document.onscroll = () =>{
+        var menu = document.getElementsByClassName("menu");
+        menu[0].classList.add("opacity");
+        console.log(window.scrollY);
+    }*/
+    if (window.scrollY != 0){
+        menu[0].classList.add("opacity");
+        
+    }else console.log("zerozerozero"); 
+
 }
