6dba36443531512cc20e2631490322e245e35df2
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-04-14 17:18:46 +0200

    completing first section -adding main content -implementing bem wherever is necessary -adding button animations -starting second section

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 window.onload = () =>{
     document.addEventListener('scroll',function(){              //script used to make the menu bar appear/disappear
         if(window.scrollY > "1"){                               // using the event listener to monitorize continually the
-            var menu = document.getElementsByClassName("menu"); //scrolling value.
+            var menu = document.getElementsByClassName("navbar"); //scrolling value.
             menu[0].classList.add("opacity");
         }else{  
-            var menu = document.getElementsByClassName("menu opacity");
+            var menu = document.getElementsByClassName("navbar opacity");
             menu[0].classList.remove("opacity");}
     });
 }
