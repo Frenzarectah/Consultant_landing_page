d0b2539c0dd2d4d1c1ef1a300a3ee8479381b4fb
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-04-15 17:29:07 +0200

    various adjusting -adjusting some fonts -adding a text change color to improve menu visibility

--- js/script.js
+++ js/script.js
@@ -2,9 +2,15 @@ window.onload = () =>{
     document.addEventListener('scroll',function(){              //script used to make the menu bar appear/disappear
         if(window.scrollY > "1"){                               // using the event listener to monitorize continually the
             var menu = document.getElementsByClassName("navbar"); //scrolling value.
-            menu[0].classList.add("opacity");
+            var menulist = document.getElementsByClassName("navbar__menu");
+            menu[0].classList.add("changes");
+            menulist[0].classList.add("change");
+
         }else{  
-            var menu = document.getElementsByClassName("navbar opacity");
-            menu[0].classList.remove("opacity");}
+            var menu = document.getElementsByClassName("navbar changes");
+            var menulist = document.getElementsByClassName("navbar__menu change");
+            menu[0].classList.remove("changes");
+            menulist[0].classList.remove("change");
+        }
     });
 }
